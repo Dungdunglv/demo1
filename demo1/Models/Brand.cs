8f6dc985e188f7aebd1602d613383c453b7a496d
dunglv <dunglv181020@gmail.com>
2024-04-21 00:58:17 +0700

    add connectionString

--- demo1/Models/Brand.cs
+++ demo1/Models/Brand.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace demo1.Models
 {
     public class Brand
     {
+        [Key] public int ID { get; set; }
+        public long BrandID { get; set; }
+        public string BrandName { get; set; }
     }
 }
